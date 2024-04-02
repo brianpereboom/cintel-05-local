f4e2a24210125852a3ea2c6433d75c6596e53077
Brian Pereboom <52140839+brianpereboom@users.noreply.github.com>
2024-04-02 11:05:28 -0700

    Update temperatures.py

--- temperatures.py
+++ temperatures.py
@@ -5,7 +5,7 @@ from datetime import datetime, timedelta
 class timeseries:
     def __init__(self, init_value=25, init_size=10, noise_scale=0.1):
         self.noise_scale = noise_scale
-        self.temps = np.cumsum(np.random.normal(size=init_size, scale=noise_scale))
+        self.temps = np.cumsum(np.random.normal(size=init_size, scale=noise_scale)) + init_value
 
     def next(self):
         self.temps = np.append(self.temps, self.temps[-1] + np.random.normal(scale=self.noise_scale))
